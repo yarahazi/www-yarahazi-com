5805686e4403571bf583311ec92ee700ac468356
Ilovecar8574 <konndoukaisei2036@gmail.com>
2026-02-09 14:17:13 +0900

    Create news-detail.js

---
+++ js/news-detail.js
@@ -0,0 +1,18 @@
+const API_KEY = "ここにAPIキー";
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
+
+fetch(`https://yarahazi.microcms.io/api/v1/news/${id}`, {
+  headers: {
+    "X-MICROCMS-API-KEY": API_KEY
+  }
+})
+  .then(res => res.json())
+  .then(item => {
+    const [y, m, d] = item.date.split("-");
+
+    document.getElementById("title").textContent = item.title;
+    document.getElementById("meta").textContent =
+      `${y}.${Number(m)}.${Number(d)} / ${item.category}`;
+    document.getElementById("body").innerHTML = item.body;
+  });
