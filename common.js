be3ec940e7554a743184b38b16b15c90b7cee242
Ilovecar8574 <konndoukaisei2036@gmail.com>
2026-02-11 15:36:27 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,13 @@
+// header
+fetch("/header.html")
+  .then(res => res.text())
+  .then(html => {
+    const header= document.getElementById("header");
+    if (header) {
+      header.innerHTML = html;
+    };
+    
+// footer
 fetch("/footer.html")
   .then(response => response.text())
   .then(html => {
