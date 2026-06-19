fb01ae4a2673ba470a9aeead82b84a4fc38c44be
やらはじ <konndoukaisei2036@gmail.com>
2026-06-19 15:00:08 +0900

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,33 +1,46 @@
 const API_KEY = "AdcI8a9AAzyT78lwCGW1IusX5cEt23fy53m9";
 const ENDPOINT = "https://yarahazi.microcms.io/api/v1/news";
 
-fetch(ENDPOINT, {
-  headers: {
-    "X-MICROCMS-API-KEY": API_KEY
-  }
-})
-  .then(res => res.json())
-  .then(data => {
-    const list = document.getElementById("news-list");
+const list = document.getElementById("news-list");
 
-    data.contents.forEach(item => {
-      const div = document.createElement("div");
-      div.className = "news-item";
-      div.onclick = () => {
-        location.href = `news-detail.html?id=${item.id}`;
-      };
+// news-list が無ければ何もしない
+if (list) {
 
-      const [y, m, d] = item.date.split("-");
+  // index.html なら3件、news.htmlなら100件取得
+  const limit = location.pathname.includes("index") || location.pathname === "/"
+    ? 3
+    : 100;
 
-      div.innerHTML = `
-        <div class="news-meta">
-          <span>${y}.${Number(m)}.${Number(d)}</span>
-          <span>${item.category}</span>
-        </div>
-        <div class="news-title">${item.title}</div>
-        <div class="news-excerpt">${item.body.slice(0, 60)}…</div>
-      `;
+  fetch(`${ENDPOINT}?limit=${limit}`, {
+    headers: {
+      "X-MICROCMS-API-KEY": API_KEY
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
 
-      list.appendChild(div);
+      data.contents.forEach(item => {
+        const div = document.createElement("div");
+        div.className = "news-item";
+
+        div.onclick = () => {
+          location.href = `news-detail.html?id=${item.id}`;
+        };
+
+        const [y, m, d] = item.date.split("-");
+
+        div.innerHTML = `
+          <div class="news-meta">
+            <span>${y}.${Number(m)}.${Number(d)}</span>
+            <span>${item.category}</span>
+          </div>
+          <div class="news-title">${item.title}</div>
+          <div class="news-excerpt">${item.body.slice(0, 60)}…</div>
+        `;
+
+        list.appendChild(div);
+      });
+
     });
-  });
+
+}
